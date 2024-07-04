43378ed9dd3c840ee646ad1559555bb2b1b78e28
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-04 12:25:45 +0530

    create footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import FooterLinks from "./footerLinks";
+
+function Footer() {
+  return (
+    <footer className="bg-[#f4f4f4] p-12 mt-12">
+      <FooterLinks />
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/footer/footerLinksData.js
@@ -0,0 +1,98 @@
+export const footerLinks = [
+  {
+    heading: "COLLECTIONS",
+    links: [
+      {
+        text: "Black teas",
+        url: "/black-teas",
+      },
+      {
+        text: "Green teas",
+        url: "/green-teas",
+      },
+      {
+        text: "White teas",
+        url: "/white-teas",
+      },
+      {
+        text: "Herbal teas",
+        url: "/herbal-teas",
+      },
+      {
+        text: "Matcha",
+        url: "/matcha",
+      },
+      {
+        text: "Chai",
+        url: "/chai",
+      },
+      {
+        text: "Oolang",
+        url: "/oolang",
+      },
+      {
+        text: "Rooibos",
+        url: "/rooibos",
+      },
+      {
+        text: "Teware",
+        url: "/teware",
+      },
+    ],
+  },
+  {
+    heading: "LEARN",
+    links: [
+      {
+        text: "About us",
+        url: "/about-us",
+      },
+      {
+        text: "About our teas",
+        url: "/about-our-teas",
+      },
+      {
+        text: "Tea academy",
+        url: "/tea-academy",
+      },
+    ],
+  },
+  {
+    heading: "CUSTOMER SERVICE",
+    links: [
+      {
+        text: "Ordering and payment",
+        url: "/ordering-and-payment",
+      },
+      {
+        text: "Delivery",
+        url: "/delivery",
+      },
+      {
+        text: "Privacy and policy",
+        url: "/privacy-and-policy",
+      },
+      {
+        text: "Terms & conditions",
+        url: "/terms-&-conditions",
+      },
+    ],
+  },
+  {
+    heading: "CONTACT US",
+    links: [
+      {
+        text: "3 Falahi, Falahi St, Pasdaran Ave, Shiraz, Fars Provieence Irant",
+        url: "/contact-us",
+      },
+      {
+        text: "Email: psm77@gmail.com",
+        url: "/email",
+      },
+      {
+        text: "Tel: +91 9173038406 ",
+        url: "/phone",
+      },
+    ],
+  },
+];
