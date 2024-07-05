ef95ea23a5a469909b3595cad80ded1714b4fb6a
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-05 13:44:08 +0530

    add classname for custom css

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
-function Button({ name, type }) {
+function Button({ name, type, className }) {
   const primary =
     "uppercase hover:bg-[#343434] font-semibold transition-colors duration-300 text-sm bg-[#282828] text-[#fff] px-12 py-3";
   const secondary = "text-sm uppercase";
 
   const buttonStyle = type === "primary" ? primary : secondary;
+
+  const combinedClassName = `${buttonStyle} ${className}`;
   return (
-    <button type={type} className={buttonStyle}>
+    <button type={type} className={combinedClassName}>
       {name}
     </button>
   );
