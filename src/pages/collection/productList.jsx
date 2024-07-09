d54101c038c122fb5fd08a5effcecd687518f841
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-09 12:08:49 +0530

    change productData to productList and creat ProductData to global

--- src/pages/collection/productList.jsx
+++ src/pages/collection/productList.jsx
@@ -1,102 +1,15 @@
-import img1 from "../../assets/img/product-img1.png";
-import img2 from "../../assets/img/product-img2.png";
-import img3 from "../../assets/img/product-img3.png";
-import img4 from "../../assets/img/product-img4.png";
-import img5 from "../../assets/img/product-img5.png";
-import img6 from "../../assets/img/product-img6.png";
-import img7 from "../../assets/img/product-img7.png";
-import img8 from "../../assets/img/product-img8.png";
-import img9 from "../../assets/img/product-img9.png";
+import { Link } from "react-router-dom";
+import { ProductData } from "./productData";
 
-function ProductData() {
-  const productData = [
-    {
-      id: 1,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img1,
-    },
-    {
-      id: 2,
-      name: "Ceylon",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 5.8,
-      weight: "50g",
-      image: img2,
-    },
-    {
-      id: 3,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img3,
-    },
-    {
-      id: 4,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img4,
-    },
-    {
-      id: 5,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img5,
-    },
-    {
-      id: 6,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img6,
-    },
-    {
-      id: 7,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img7,
-    },
-    {
-      id: 8,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img8,
-    },
-    {
-      id: 9,
-      name: "Ceylon Ginger",
-      lastName: "Cinnamon chai tea",
-      currency: "$",
-      rate: 4.8,
-      weight: "50g",
-      image: img9,
-    },
-  ];
-
+function ProductList() {
   return (
     <div className="mt-10 grid grid-cols-3 gap-5 place-items-center text-center">
-      {productData.map((item, index) => (
-        <div key={index} className="hover:underline cursor-pointer">
+      {ProductData.map((item, index) => (
+        <Link
+          key={index}
+          to={`/collection/productinfo/${item.id}`}
+          className="hover:underline cursor-pointer"
+        >
           <div className="overflow-hidden">
             <img
               className="transition-all duration-300 hover:scale-110"
@@ -109,10 +22,10 @@ function ProductData() {
           <p className="text-sm mt-1 capitalize">
             {item.currency + item.rate + "/" + item.weight}
           </p>
-        </div>
+        </Link>
       ))}
     </div>
   );
 }
 
-export default ProductData;
+export default ProductList;
