0b917216eb26b59f895688fc73371d0d2292bd00
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-12 17:41:18 +0530

    create and update routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Collection from "./pages/collection/collection";
 import User from "./pages/user";
 import CreateAccountForm from "./pages/createAccount";
 import ForgetPasswordForm from "./pages/resetPassword";
+import ProductInfo from "./pages/productInfo";
+import Cart from "./pages/cartPage";
+import { PageNotFound } from "./pages/pageNotFound";
+import Success from "./pages/success";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="collection" element={<Collection />} />
-        <Route path="user" element={<User />} />
-        <Route path="cart" element="cart" />
-        <Route path="search" element="searchbar" />
-        <Route path="createAccount" element={<CreateAccountForm />} />
-        <Route path="forgetPassword" element={<ForgetPasswordForm />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/collection/productinfo/:id" element={<ProductInfo />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/createAccount" element={<CreateAccountForm />} />
+        <Route path="/forgetPassword" element={<ForgetPasswordForm />} />
+        <Route path="/thanks" element="thanks" />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </BrowserRouter>
   );
