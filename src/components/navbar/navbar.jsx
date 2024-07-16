42e18c6a0a849ef03900e75945568b6d2a855545
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-16 17:45:07 +0530

    write in semantic way

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,29 +2,32 @@ import { FaSearch, FaUser } from "react-icons/fa";
 import { FaBagShopping } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const styleLi = "cursor-pointer hover:underline";
+const classLi = "hover:underline";
 
 function Navbar() {
   return (
     <header className="flex justify-between items-center p-10">
-      <Link to="/">
-        <h2 className="font-semibold text-2xl cursor-pointer">Time for Tea</h2>
+      <Link to="/" className="font-semibold text-2xl">
+        Time for Tea
       </Link>
 
       <nav>
         <ul className="flex gap-8 text-sm">
-          <Link to="/collection">
-            <li className={styleLi}>TEA COLLECTIONS</li>
-          </Link>
-          <Link to="/accessories">
-            <li className={styleLi}>ACCESSORIES</li>
-          </Link>
-          <Link to="/blog">
-            <li className={styleLi}>BLOG</li>
-          </Link>
-          <Link to="/contact">
-            <li className={styleLi}>CONTACT US</li>
-          </Link>
+          <li className={classLi}>
+            <Link to="/collection">TEA COLLECTIONS</Link>
+          </li>
+
+          <li className={classLi}>
+            <Link to="/accessories">ACCESSORIES </Link>
+          </li>
+
+          <li className={classLi}>
+            <Link to="/blog">BLOG </Link>
+          </li>
+
+          <li className={classLi}>
+            <Link to="/contact">CONTACT US </Link>
+          </li>
         </ul>
       </nav>
       <div className="flex gap-8 cursor-pointer">
