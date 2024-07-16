9266d6b91ce841e10104b5134867dd6b162b1bce
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-16 17:52:12 +0530

    remove extra file and update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,23 @@
-import FooterLinks from "./footerLinks";
+import { Link } from "react-router-dom";
+import { footerLinks } from "./footerLinksData";
 
 function Footer() {
   return (
-    <footer className="bg-[#f4f4f4] p-12 mt-12">
-      <FooterLinks />
+    <footer className="bg-[#f4f4f4] p-12 mt-12 flex justify-around text-sm">
+      {footerLinks.map((section, index) => (
+        <div key={index}>
+          <h2 className="uppercase font-semibold mb-2">{section.heading}</h2>
+          <ul className="flex flex-col gap-2">
+            {section.links.map((link, linkIndex) => (
+              <li key={linkIndex}>
+                <Link to={link.url} className="hover:underline">
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </footer>
   );
 }
