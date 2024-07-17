59232cc93181588088e979f5bba13be505c0e18c
PraveenMahra <praveen.mehra65@gmail.com>
2024-07-17 16:10:47 +0530

    make responsive for all screens

--- src/components/section/introduction.jsx
+++ src/components/section/introduction.jsx
@@ -4,12 +4,12 @@ import Button from "../button/button";
 
 function Introduction() {
   return (
-    <div className="flex items-center gap-20">
-      <div className="w-[500px]">
+    <div className="flex flex-col sm:flex-row items-center  gap-20">
+      <div className="lg:w-[500px]">
         <img src={landingImg} alt="img" />
       </div>
-      <div className="w-[700px]">
-        <h2 className="font-semibold text-xl">
+      <div className="lg:w-[700px] sm:w-[500px] sm:text-left sm:px-0 text-center px-2">
+        <h2 className="font-semibold sm:text-xl text-lg">
           Every day is unique, just like our tea
         </h2>
         <p className="my-4">
